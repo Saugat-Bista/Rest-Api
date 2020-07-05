1f72d5f741f2a224584f38e48eb5f4262038e6f7
Saugat <saugatbista052@gmail.com>
2020-07-05 15:52:16 -0400

    Added PostgreSQL

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from db import db
 
 from security import auth, identity
 from resources.user import UserRegister
@@ -9,7 +11,7 @@ from datetime import timedelta
 from resources.store import Store, Stores
 
 app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXPECTATIONS'] = True
 app.secret_key= 'abc'
@@ -26,4 +28,5 @@ api.add_resource(Stores, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
